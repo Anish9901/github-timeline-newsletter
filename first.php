ebc4ce36380e490d51e9925eca90d2f800e5b919
Anish <umaleanish120@gmail.com>
2021-12-13 03:18:40 +0530

    Made changes

--- first.php
+++ first.php
@@ -1,17 +1,58 @@
-<?php
-//$api_url = "https://github.com/timeline";
+<html>
+  <head>
+    <title>
+      GitHub Timleine Updates.
+    </title>
+  </head>
+  <body>
+  <h1>GitHub Timleine Updates</h1>
+  <?php
+  $aa = 'hello';
+  echo '<a style="text-decoration: none;" href="https://google.com">'.$aa.'</a>';
+/* //$api_url = "https://github.com/timeline";
 //$my_var = file_get_contents('test.xml');
-//echo $my_var;
-$xml = simplexml_load_file('https://github.com/timeline') or die("Cant load xml");
+//echo $my_var; */
+//$xml = '<xml><test><a a="b" r="x" q="v" /></test><b/></xml>';
+//$x = new SimpleXMLElement($xml);
+$xml = simplexml_load_file('test.xml') or die("Cant load xml");
 //print_r($xml);
-foreach($xml as $record)
+//echo "hello";
+
+foreach($xml->entry as $record)
 {
-	echo "\n";
-    echo $record->id."\n";
-echo $record->content."\n\n";
+  //print_r($record);
+	/* $show = $record->link->attributes();
+  //print_r($show);
+  
+  echo $show['href'];
+  echo "<br>";
+  echo $record->title;
+  echo "<br>"; */
+  $yo = $record->title;
+  $str = explode(" ",$yo);
+  echo $str[0]." ";
+  echo $str[count($str)-1]."<br>";
+  /* echo $record->author->name;
+  echo "<br>";
+  echo $record->author->uri;
+  echo "<br>";
+  //echo $record->author->uri;
+  echo $record->content."\n\n"; */
 }
-//foreach($xml as $record)
-//{
-//    print_r($record);
-//}
+
+
+/*  foreach($xml as $record)
+{ 
+    print_r($record);
+} */
+/* $attr = $x->test[0]->a[0]->attributes();
+echo $attr['a']; */
+//print_r($attr)
+
+//print_r($xml)
+
 ?>
+</body>
+</html>
+
+
